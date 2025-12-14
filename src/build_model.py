b021a89b8abf6918208be6471ba59fc8cc7ab044
Daniel Seichter <daniel.seichter@tu-ilmenau.de>
2025-12-14 16:29:42 +0100

    fix mps in build model

--- src/build_model.py
+++ src/build_model.py
@@ -89,6 +89,8 @@ def build_model(args, n_classes):
 
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
     else:
         device = torch.device("cpu")
 
